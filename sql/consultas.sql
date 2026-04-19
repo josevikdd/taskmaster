95289b38b6637463bf99f1334d1a8699854bce53
josevikdd <josevi97sgb@gmail.com>
2026-04-19 15:33:06 +0200

    Modificar consultas.sql
    Añadida una segunda versión para la consulta de tareas por estado en la cual se selecciona un único estado con una función PL/pgSQL.

--- sql/consultas.sql
+++ sql/consultas.sql
@@ -1,10 +1,38 @@
---Mostrar las tareas por estado--
+--1.1. Mostrar las tareas por estado (tareas ordenadas)--
 SELECT *
 	FROM tarea
 	ORDER BY id_estado
 
+--1.2. Mostrar las tareas por estado (seleccionar un estado con una función)--
+--1.2.1.Función--
+CREATE OR REPLACE FUNCTION seleccionEstado(seleccion VARCHAR) RETURNS int AS
+$$
+DECLARE
 
---Mostrar las tareas completadas--
+BEGIN
+	IF seleccion = 'Pendiente'
+		THEN RETURN 1;
+	END IF;
+	IF seleccion = 'En progreso'
+		THEN RETURN 2;
+	END IF;
+	IF seleccion = 'Completada'
+		THEN RETURN 3;
+	END IF;
+	IF seleccion = 'Cancelada'
+		THEN RETURN 4;
+	END IF;
+END;
+$$
+language PLPGSQL;
+
+--1.2.2. Consulta SQL--
+SELECT *
+	FROM tarea
+	WHERE id_estado = seleccionEstado('En progreso')
+
+
+--2. Mostrar las tareas completadas--
 SELECT *
 	FROM tarea
 	WHERE id_estado = 3
