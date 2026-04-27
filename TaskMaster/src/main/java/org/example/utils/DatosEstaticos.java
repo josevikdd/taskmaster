7e0d035a4e57b722be3f951d2c75fa26e265f640
José Vicente Sánchez Vargues <josevi97sgb@gmail.com>
2026-04-27 18:54:52 +0200

    Modificar DatosEstaticos.java para su correcta importación al iniciar el programa.

--- TaskMaster/src/main/java/org/example/utils/DatosEstaticos.java
+++ TaskMaster/src/main/java/org/example/utils/DatosEstaticos.java
@@ -1,5 +1,6 @@
 package org.example.utils;
 
+import org.example.main.TaskMaster;
 import org.example.model.Categoria;
 import org.example.model.Estado;
 
@@ -10,9 +11,15 @@ public class DatosEstaticos {
         Estado e2 = new Estado(2, "En progreso.", "La tarea ha comenzado.");
         Estado e3 = new Estado(3, "Finalizada.", "La tarea ha sido finalizada.");
         Estado e4 = new Estado(4, "Cancelada.", "La tarea ha sido cancelada.");
+        TaskMaster.getEstados().add(e1);
+        TaskMaster.getEstados().add(e2);
+        TaskMaster.getEstados().add(e3);
+        TaskMaster.getEstados().add(e4);
 
         //Creamos las categorías básicas del proyecto.
-        Categoria c1 = new Categoria(111, "Deberes DAM.", "Categoría para los deberes del curso DAM.");
-        Categoria c2 = new Categoria(222, "Tiempo libre.", "Categoría para actividades que no tienen que ver con los estudios de DAM.");
+        Categoria c1 = new Categoria(1, "Deberes DAM.", "Categoría para los deberes del curso DAM.");
+        Categoria c2 = new Categoria(2, "Tiempo libre.", "Categoría para actividades que no tienen que ver con los estudios de DAM.");
+        TaskMaster.getCategorias().add(c1);
+        TaskMaster.getCategorias().add(c2);
     }
 }
